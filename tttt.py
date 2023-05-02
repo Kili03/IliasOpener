4e406352dbdf630cf3638a2410369ea4653aacaa
Kili03 <kilian.bechtold@mail.com>
2023-05-02 22:13:06 +0200

    added adding to file via terminal

--- tttt.py
+++ tttt.py
@@ -1,3 +1,47 @@
+import json
+
+with open("courses.json", "r") as f:
+    courses = json.load(f)
+
+check = input("Course to add/edit: ")
+if check in courses:
+    name = input()
+    while name != "end":
+        if name not in courses[check]:
+            id = input()
+            courses[check][name] = id
+            name = input()
+        else:
+            print(f"{name} is already there")
+            name = input()
+
+  
+else:
+    b = input(f"Add course {check} [y/n]")
+    if b == "y":
+        id = input("Course Id: ")
+        courses[check] = {"id" : id}
+
+        name = input()
+        while name != "end":
+            if name not in courses[check]:
+                id = input()
+                courses[check][name] = id
+                name = input()
+            else:
+                print(f"{name} is already there")
+                name = input()
+
+    else:
+        print(f"didn't add course {check}")
+
+
+
+with open("courses.json", "w") as f:
+    json.dump(courses, f)
+
+
+'''
 names = []
 codes = []
 
@@ -19,4 +63,4 @@ for i in range(len(names)):
     else:
         addToDict += f"    \"{n}\" : \"{c}\""
 
-print(addToDict)
+print(addToDict)'''
